f7be6e6a9b30555621c47219ecc3161e964ff947
HyungJu <sungkisa@naver.com>
2020-07-26 05:32:36 +0900

    style: 메인 페이지 예상 평점 부분 과도한 마진 제거

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,21 +9,20 @@
       </div>
 
       <div class="scrolling-wrapper">
-        <div class="q-mr-lg q-mb-md" v-for="element in section.data">
+        <div class="q-mr-lg q-mb-md" v-for="element in section.data" @click="$router.push({ name: 'Detail', params: {item: element}})">
           <div style="width: 130px; height: 130px; background-color: #FAFAFA; border-radius: 10px; display: flex; align-items: center; justify-content: center; margin-top: 10px" >
             <q-img
               v-bind:src="element.image"
               width="120px"
             />
           </div>
-          <h6 style="margin-top: 10px; font-size: 14px;line-height: 16px; max-width: 120px;    text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">{{element.name}}</h6>
-          <p style="margin-bottom: 0px">예상 : {{element.predict?element.predict.toFixed(1)+"점":"준비중"}}</p> <p>평균 : {{element.average?element.average.toFixed(1)+"점":"준비중"}}</p>
+          <h6 style="margin-top: 10px; margin-bottom: 3px;font-size: 14px;line-height: 16px; max-width: 120px;    text-overflow: ellipsis;overflow: hidden;white-space: nowrap;">{{element.name}}</h6>
+          <p style="margin-bottom: 0px; color: #838383">예상 : {{element.predict?element.predict.toFixed(1)+"점":"준비중"}}</p> <p style="color: #838383">평균 : {{element.average?element.average.toFixed(1)+"점":"준비중"}}</p>
         </div>
 
       </div>
 
     </div>
-
   </q-page>
 </template>
 <style>
